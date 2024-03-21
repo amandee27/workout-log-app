9d92cd3baed98272447d266e9201ca6b577a7b5d
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2024-03-21 18:30:18 +0000

    Add login button to Nav Bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import {useState, useEffect} from 'react';
+
 
 const NavBar = () => {
+
+    const [loggedIn, set] = useState(JSON.parse(localStorage.getItem('token')));
+
+   const logout = () => {
+    localStorage.removeItem('login');
+    localStorage.removeItem('token');
+    window.location.reload();
+   }
+
+
+
     return ( 
         <nav className="navbar">
             <div className="links">
@@ -8,8 +21,10 @@ const NavBar = () => {
                 <Link to="/workout-history">Workout History</Link>
                 <Link to="/workout-summary">Workout Summary</Link>
                 <Link to="/weight-summary">Weight Summary</Link>
-                
                 <Link to="/weight-summary"><b>User Detail</b></Link>
+                {!loggedIn && <Link to="/login">Login</Link>}
+                {loggedIn && <button onClick={logout}>Logout</button>}
+                
             </div>
         </nav>
      );
