10ea43f9b0bc849f07a22c46e0dadff23650851c
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2024-06-13 23:51:08 +0100

    Move nav bar in to the protected route component

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,9 +1,14 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom';
+import NavBar from './NavBar/NavBar';
 
 const ProtectedRoute = () => {
+    console.log("Loading Protected route");
     let auth = {'token': localStorage.getItem('token') || false}
     return ( 
-        auth.token ? <Outlet/> : <Navigate to="/login"/>
+        auth.token ? <div>
+            <NavBar></NavBar> {/*Navbar moved to here from app.js */}
+            <Outlet/>
+        </div>  : <Navigate to="/login"/>
     
      );
 }
