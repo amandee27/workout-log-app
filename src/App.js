26fbcb5171599a6fce993d5fcfc4ed02ca317938
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2024-06-13 23:49:57 +0100

    Moved nav bar in to the protected route component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import Home from './Home';
-import NavBar from './NavBar';
 import UserDetail from './UserDetail';
 import Users from './Users';
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
-import WorkoutCategory from './WorkoutCategory';
-import LoggedWorkout from './LoggedWorkout';
+import WorkoutCategory from './WorkoutCategory/WorkoutCategory';
+import LoggedWorkout from './LoggedWorkout/LoggedWorkout';
 import WorkoutSummmary from './WorkoutSummary';
-import WeightMonitor from './WeightMonitor';
+import WeightMonitor from './Weight Monitor/WeightMonitor';
 import Login from './Login';
 import SignIn from './Sign-In';
 import PageNotFound from './PageNotFound';
 import { useState, useEffect } from 'react';
 import './App.css';
 import ProtectedRoute from './ProtectedRoute';
+import CoreTrainingCategory from './WorkoutCategory/CoreTraining/CoreTrainingCategory';
+import IntensityTrainingCategory from './WorkoutCategory/IntensityTraining/IntensityTrainigCategory';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyList from './MyList';
+import PlannedWorkout from './PlannedWorkout/PlannedWorkout';
+import { BrowserRouter } from 'react-router-dom';
+import AddWorkout from './AddWorkout/AddWorkout';
+import LoggedWorkoutDetail from './LoggedWorkout/LoggedWorkoutDetails/LoggedWorkoutDetails';
+
 
 function App() {
   /*
@@ -29,7 +37,7 @@ function App() {
   //const { pathname } = useLocation();
   const [loggedIn, setLoggedIn] = useState(JSON.parse(localStorage.getItem('token')));
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+
 
 
 
@@ -71,38 +79,51 @@ function App() {
   
     */
 
-    useEffect(() => {
-      if(loggedIn){
-        setIsLoggedIn(true);
-      }
-    }, [loggedIn])
+  /** Disabled due render component twice. check the app functionality */
 
-     
+  // useEffect(() => {
+  //   if(loggedIn){
+  //     setIsLoggedIn(true);
+  //   }
+  //   console.log(isLoggedIn)
+  // }, [loggedIn])
 
-    const handleLogin = () => {
-      // Perform login logic
-      setIsLoggedIn(true);
-      console.log("Calling from login ", isLoggedIn);
-    };
+
+
+  const handleLogin = () => {
+    // Perform login logic
+    setIsLoggedIn(true);
+    console.log("Calling from login ", isLoggedIn);
+  };
+
+  console.log("Calling app.js");
 
   return (
     <div>
-      {isLoggedIn && <NavBar></NavBar>}
+      {/*isLoggedIn && <NavBar></NavBar> */}
       <div>
-        <Routes>
-          <Route element={<ProtectedRoute />}>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route exact path="/users" element={<Users />}></Route>
-            <Route exact path="/users/:id" element={<UserDetail />}></Route>
-            <Route exact path='/workout-history' element={<LoggedWorkout />}></Route>
-            <Route exact path='/workout-type' element={<WorkoutCategory />}></Route>
-            <Route exact path='/workout-summary' element={<WorkoutSummmary />}></Route>
-            <Route path='/weight-summary' element={<WeightMonitor />}></Route>
-          </Route>
-          <Route path='/login' element={<Login onLogin={handleLogin} />}></Route>
-          <Route path='/sign-up' element={<SignIn />}></Route>
-          <Route path='*' element={<PageNotFound />}></Route>
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<ProtectedRoute />}>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/users" element={<Users />}></Route>
+              <Route exact path="/users/:id" element={<UserDetail />}></Route>
+              <Route exact path='/workout-history' element={<LoggedWorkout />}></Route>
+              <Route exact path='/workout-type' element={<WorkoutCategory />}></Route>
+              <Route exact path='/workout-summary' element={<WorkoutSummmary />}></Route>
+              <Route path='/weight-summary' element={<WeightMonitor />}></Route>
+              <Route path='/core-training' element={< CoreTrainingCategory />}></Route>
+              <Route path='/intensity-training' element={< IntensityTrainingCategory />}></Route>
+              <Route path='/my-list' element={<MyList />}></Route>
+              <Route path='/calendar' element={<PlannedWorkout />}></Route>
+              <Route path='/add-workout' element={<AddWorkout />}></Route>
+              <Route path='/logged-workout' element={<LoggedWorkoutDetail />}></Route>
+            </Route>
+            {!isLoggedIn && <Route path='/login' element={<Login onLogin={handleLogin} />}></Route>}
+            <Route path='/sign-up' element={<SignIn />}></Route>
+            <Route path='*' element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter> {/* BrowserRouter moved to here from app.js. since NavBar no longer available in here */}
       </div>
     </div>
 
