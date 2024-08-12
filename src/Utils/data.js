795fc797493f9559ae238d30b3e489e97d8a12a5
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2024-08-12 16:55:08 +0100

    Sample data

---
+++ src/Utils/data.js
@@ -0,0 +1,38 @@
+export const Data = [
+    {
+        id: 1,
+        year: new Date("2021-11-24 12:03:00"),//12:03:00
+        userWeight: 57,
+        bodyFat: 60
+      },
+      {
+        id: 2,
+        year: new Date("2021-11-25 13:03:00"), //13:03:00
+        userWeight: 57,
+        bodyFat: 65
+      },
+      {
+        id: 3,
+        year:  new Date("2021-11-26 16:03:00"),//16:03:00
+        userWeight: 56.5,
+        bodyFat: 63
+      },
+      {
+        id: 4,
+        year: new Date("2021-11-27 16:03:00"),//16:03:00
+        userWeight: 56,
+        bodyFat: 60
+      },
+      {
+        id: 5,
+        year: new Date("2021-11-28 16:03:00") ,
+        userWeight: 56,
+        bodyFat: 58
+      },
+      {
+        id: 6,
+        year: new Date("2022-02-28 16:03:00") ,
+        userWeight: 55,
+        bodyFat: 50
+      }
+];
