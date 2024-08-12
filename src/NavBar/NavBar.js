2578f5848b0bed68751eadbca3bd366cbf0ec141
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2024-08-12 16:43:04 +0100

    Restructure the navbar folder and make changes

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import {useState, useEffect} from 'react';
+import './NavBar.css'
 
 
 const NavBar = () => {
@@ -18,10 +19,13 @@ const NavBar = () => {
         <nav className="navbar">
             <div className="links">
                 <Link to="/workout-type">Workout Catagory</Link>
-                <Link to="/workout-history">Workout History</Link>
-                <Link to="/workout-summary">Workout Summary</Link>
-                <Link to="/weight-summary">Weight Summary</Link>
-                <Link to="/weight-summary"><b>User Detail</b></Link>
+                <Link to="/workout-history">Calendar</Link>
+                <Link to="/workout-summary">Scheduler</Link>{/*exercise tracker, Exercise log, */}
+                <Link to="/weight-summary">Progress</Link>
+                <Link to="/users/:id"><b>User Profile</b></Link>
+                <Link to="/test">TEST</Link>
+                {/*<Link to="/my-list"><b>My List</b></Link> */}
+                {/*<Link to="/calendar"><b>Workout plan</b></Link>*/}
                 {!loggedIn && <Link to="/login">Login</Link>}
                 {loggedIn && <button onClick={logout}>Logout</button>}
                 
