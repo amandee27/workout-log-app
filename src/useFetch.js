6ee52aff6fa63f93d864b2cf103ef890de5dd07d
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2024-08-12 16:53:35 +0100

    Modifications in useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -3,12 +3,16 @@ import { useEffect, useState } from "react"
 const useFetch = (url) => {
 
     const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
     // const [error, setError] = useState(null);
 
     useEffect(()=>{
+        const abortConst = new AbortController();
+
 
         setTimeout(()=>{
-            fetch(url).then(res => {
+            fetch(url, {signal: abortConst.signal}).then(res => {
                 if(!res.ok){
                     console.log(res);
                 }
@@ -16,13 +20,16 @@ const useFetch = (url) => {
             }).then(data => {
                 console.log(data);
                 setData(data);
+                setIsPending(false);
             }).catch(err=>{
                 console.log(err.message);
+                console.log(err);
+                setError(err.message)
                 //setError(err.message);
             })
         },1000);
     },[url])
-    return {data};
+    return {data, isPending, error};
 }
 
 
